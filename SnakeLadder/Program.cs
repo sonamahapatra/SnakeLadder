7092242049c3e0067fde085b7c2e28489dd99c8f
sonamahapatra <sonalimahapatra105@gmail.com>
2023-04-12 23:44:38 +0530

    [sonamahapatra] Added Welcome message

---
+++ SnakeLadder/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnakeLadder
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome To the Snake And Ladder Program");
+            Console.ReadLine();
+        }
+    }
+}
